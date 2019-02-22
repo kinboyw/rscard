ea6510f1e2f17495eed2a4abca3c4f3a51118cab
wangjinbo <vannue@qq.com>
2019-02-22 15:16:03 +0800

    update

--- js/welcome.js
+++ js/welcome.js
@@ -1,9 +1,8 @@
 !function(){    //加载动画
     window.onload = function(){
-        var time = Math.random()*2000;
         setTimeout(() => {
             siteWelcome.classList.remove("active");
             document.querySelectorAll('[data-x]')[0].classList.remove("offset");//第一板块自动上浮
-        }, time);
+        },0);
     }
 }.call()
